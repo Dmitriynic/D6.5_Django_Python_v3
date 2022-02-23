fa6c70e722063e4fcec0313daabff08436678a6e
Dmitriynic <67915633+Dmitriynic@users.noreply.github.com>
2022-02-23 23:09:20 +0300

    Add files via upload

---
+++ NewsPaper/news/apps.py
@@ -0,0 +1,16 @@
+from django.apps import AppConfig
+
+
+class NewsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'news'
+
+    def ready(self):
+        import news.signals
+ 
+class PostConfig(AppConfig):
+    name = 'news1'
+ 
+    # нам надо переопределить метод ready, чтобы при готовности нашего приложения импортировался модуль со всеми функциями обработчиками
+    def ready(self):
+        import news.signals
